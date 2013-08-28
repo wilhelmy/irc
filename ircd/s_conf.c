8b9cb4373794284fb5591cef86c8d4bba775c3a1
sd <sd@hysteria.cz>
2013-08-28 17:20:03 +0200

    ACL support for /encap
    Signed-off-by: sd <sd@hysteria.cz>

--- ircd/s_conf.c
+++ ircd/s_conf.c
@@ -256,6 +256,8 @@ char	*oline_flags_to_string(long flags)
 		*s++ ='D';
 	if (flags & ACL_SET)
 		*s++ ='e';
+	if (flags & ACL_ENCAP)
+		*s++ ='E';
 	if (flags & ACL_TKLINE)
 		*s++ ='T';
 	if (flags & ACL_KLINE)
@@ -306,6 +308,7 @@ long	oline_flags_parse(char *string)
 		case 'R': tmp |= ACL_RESTART; break;
 		case 'D': tmp |= ACL_DIE; break;
 		case 'e': tmp |= ACL_SET; break;
+		case 'E': tmp |= ACL_ENCAP; break;
 		case 'T': tmp |= ACL_TKLINE; break;
 		case 'q': tmp |= ACL_KLINE; break;
 #ifdef CLIENTS_CHANNEL
@@ -348,6 +351,9 @@ long	oline_flags_parse(char *string)
 #endif
 #ifndef OPER_SET
 	tmp &= ~ACL_SET;
+#endif
+#ifndef OPER_ENCAP
+	tmp &= ~ACL_ENCAP;
 #endif
 #ifndef OPER_TKLINE
 	tmp &= ~ACL_TKLINE;
--- ircd/s_serv.c
+++ ircd/s_serv.c
@@ -3900,8 +3900,8 @@ int	m_encap(aClient *cptr, aClient *sptr, int parc, char *parv[])
 		if (parc < 5 || strcmp(parv[2], "PARSE") || !encap_whitelisted(parv[4]))
 			return 0;
 	} else if (MyClient(cptr)) {
-		/* For now .. enforced in parse.c handler too */
-		if (!IsAnOper(sptr))
+		/* For now .. hopefully is_allowed() will work for remote too, some day */
+		if (is_allowed(sptr, ACL_ENCAP))
 			return m_nopriv(cptr, sptr, parc, parv);
 
 		/* With great power comes great responsibility.
